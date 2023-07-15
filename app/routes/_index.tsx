6892a3ea87a9e3a8db11defe3f298cef8e986f6e
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-15 21:17:26 +0700

    chore(remix): create remix app

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>PyCon Indonesia 2023</h1>
+    </div>
+  );
+}
