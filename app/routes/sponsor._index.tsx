2c7bedc016b3192729e5cb0e4e53d34e67e9f671
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-07-22 23:33:50 +0700

    feat: create benefit cards

--- app/routes/sponsor._index.tsx
+++ app/routes/sponsor._index.tsx
@@ -7,6 +7,95 @@ export const meta: V2_MetaFunction = () => {
   return [{ title: "Sponsor" }]
 }
 
+const colorMap = {
+  red: {
+    card: "border-primary-red",
+    shadow: "bg-primary-red",
+  },
+  purple: {
+    card: "border-primary-purple",
+    shadow: "bg-primary-purple",
+  },
+  black: {
+    card: "border-primary-black",
+    shadow: "bg-primary-black",
+  },
+  orange: {
+    card: "border-primary-orange",
+    shadow: "bg-primary-orange",
+  },
+} as const
+
+const SPONSORSHIP_BORDER_COLORS = Object.keys(colorMap) as Array<
+  keyof typeof colorMap
+>
+
+const benefits = [
+  {
+    title: "Recruiting",
+    description:
+      "What better place to find Python developers than a gathering of 250 of them? If you’re hiring, PyCon is the place to be. The conference has long been a great way to find the talented developers so many companies are searching for, so much that we added on-site Job Fair to make it even easier.",
+  },
+  {
+    title: "Branding",
+    description:
+      "It is an opportunity to appeal that your company is technology-oriented organization. The posting of the logo will be recorded by movie and will be broadcasted on the web-media afterwards, leading to branding of the company and products. Continuous exposure of names and logos of companies, products, and services can generate many merits even after this conference, such as appeal to participants, topicality and exciting experience.",
+  },
+  {
+    title: "Marketing",
+    description:
+      "It's a great chance to advertise your products and introduce detailed usages directly to Python engineers and Python users. Specifically, it is possible to have more interaction to participants by having a booth and directly explaining, having a talk session, and by distributing leaflets and goods.",
+  },
+  {
+    title: "Audience",
+    description:
+      "PyCon’s ever diversifying audience puts your organization in front of a wide variety of people. From beginners to experts, PyCon draws attendees from those with zero experience all the way through a significant group of the contributors to the language itself. We also see people of all levels from many industries. From developers to managers, executives to owners, startups to big businesses, PyCon draws attendees from all sorts of places. Some organizations send entire teams, and in the case of many startups, the entire company may be there.",
+  },
+  {
+    title: "CSR",
+    description:
+      "Sponsoring can show your gratitude for Python community. It's one of CSR activity that supports technology companies and engineers community.",
+  },
+  {
+    title: "Flexibility",
+    description:
+      "If you want to help PyCon, we want to help you construct a sponsorship package that fits your needs. If a small booth would work better than a large one, perhaps we can swap it in exchange for another benefit that works better for your organization. For some organizations, a booth is not practical at all, but adding another conference pass may work better.",
+  },
+]
+
+type SponsorshipCardProps = React.HTMLAttributes<HTMLDivElement> & {
+  title: string
+  color?: keyof typeof colorMap
+}
+
+function SponsorshipCard({
+  title,
+  color = "red",
+  children,
+  className,
+}: SponsorshipCardProps) {
+  return (
+    <div className="relative">
+      <div
+        className={cn(
+          "relative z-0 h-full rounded-xl border-2 bg-white p-6",
+          colorMap[color].card,
+          className,
+        )}
+      >
+        <h4>{title}</h4>
+        <p>{children}</p>
+      </div>
+      <div
+        className={cn(
+          "absolute -left-2 top-2 -z-10 h-full w-full rounded-xl",
+          colorMap[color].shadow,
+        )}
+      />
+    </div>
+  )
+}
+
 export default function Index() {
   return (
     <Layout>
@@ -35,76 +124,36 @@ export default function Index() {
       <article
         className={cn(
           "prose prose-lg mx-auto mt-20 w-full max-w-6xl px-4",
-          "prose prose-h3:mb-16 prose-h3:text-center prose-h3:text-3xl md:prose-h3:text-4xl",
-          "prose prose-h4:mb-5 prose-h4:mt-12 prose-h4:text-2xl md:prose-h4:text-3xl",
-          "prose-p:text-base prose-p:leading-normal prose-p:tracking-wide md:prose-p:text-xl",
+          "prose-h3:mb-16 prose-h3:text-center prose-h3:text-5xl prose-h3:font-normal md:prose-h3:text-5xl md:prose-h3:leading-normal",
+          "prose-h4:mb-5 prose-h4:mt-0 prose-h4:text-2xl md:prose-h4:text-3xl",
+          "prose-p:text-justify prose-p:text-base prose-p:leading-normal prose-p:tracking-wide md:prose-p:text-base",
           "md:prose-lg",
         )}
       >
         <h3>
-          Sponsorship <span className="text-primary">Prospect</span> and{" "}
+          Sponsorship <br /> <span className="text-primary">Prospect</span> and{" "}
           <span className="text-primary">Benefit</span>
         </h3>
-        <p>
+        <p className="mx-auto max-w-2xl">
           What better place to find Python developers than a gathering of 250 of
           them? If you’re hiring, PyCon is the place to be. The conference has
           long been a great way to find the talented developers so many
           companies are searching for, so much that we added on-site Job Fair to
           make it even easier.
         </p>
-        <h4>Recruiting</h4>
-        <p>
-          What better place to find Python developers than a gathering of 250 of
-          them? If you’re hiring, PyCon is the place to be. The conference has
-          long been a great way to find the talented developers so many
-          companies are searching for, so much that we added on-site Job Fair to
-          make it even easier.
-        </p>
-        <h4>Branding</h4>
-        <p>
-          It is an opportunity to appeal that your company is
-          technology-oriented organization. The posting of the logo will be
-          recorded by movie and will be broadcasted on the web-media afterwards,
-          leading to branding of the company and products. Continuous exposure
-          of names and logos of companies, products, and services can generate
-          many merits even after this conference, such as appeal to
-          participants, topicality and exciting experience.
-        </p>
-        <h4>Marketing</h4>
-        <p>
-          It's a great chance to advertise your products and introduce detailed
-          usages directly to Python engineers and Python users. Specifically, it
-          is possible to have more interaction to participants by having a booth
-          and directly explaining, having a talk session, and by distributing
-          leaflets and goods.
-        </p>
-        <h4>Audience</h4>
-        <p>
-          PyCon’s ever diversifying audience puts your organization in front of
-          a wide variety of people. From beginners to experts, PyCon draws
-          attendees from those with zero experience all the way through a
-          significant group of the contributors to the language itself. We also
-          see people of all levels from many industries. From developers to
-          managers, executives to owners, startups to big businesses, PyCon
-          draws attendees from all sorts of places. Some organizations send
-          entire teams, and in the case of many startups, the entire company may
-          be there.
-        </p>
-        <h4>CSR</h4>
-        <p>
-          Sponsoring can show your gratitude for Python community. It's one of
-          CSR activity that supports technology companies and engineers
-          community.
-        </p>
-        <h4>Flexibility</h4>
-        <p>
-          If you want to help PyCon, we want to help you construct a sponsorship
-          package that fits your needs. If a small booth would work better than
-          a large one, perhaps we can swap it in exchange for another benefit
-          that works better for your organization. For some organizations, a
-          booth is not practical at all, but adding another conference pass may
-          work better.
-        </p>
+        <div className="mt-16 grid grid-cols-2 gap-6">
+          {benefits.map((benefit, i) => (
+            <SponsorshipCard
+              key={`benefit-${i}`}
+              title={benefit.title}
+              color={
+                SPONSORSHIP_BORDER_COLORS[i % SPONSORSHIP_BORDER_COLORS.length]
+              }
+            >
+              {benefit.description}
+            </SponsorshipCard>
+          ))}
+        </div>
       </article>
     </Layout>
   )
