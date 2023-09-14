97de9de30857a5f828aa068a22fc47dc80486f29
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-09-14 13:16:19 +0700

    feat: remove use client directives

--- app/components/ui/datepicker.tsx
+++ app/components/ui/datepicker.tsx
@@ -13,7 +13,7 @@ import {
   PopoverTrigger,
 } from "./popover"
 
-export function DatePickerDemo() {
+export function DatePicker() {
   const [date, setDate] = React.useState<Date>()
 
   return (
