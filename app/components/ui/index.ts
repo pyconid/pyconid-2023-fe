aa0184bf8506be1c9ca2b06c6451fcb90a5be101
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-09-16 18:34:35 +0700

    feat: add primitives (#68)
    * feat: add primitives input, select, checkbox
    
    * feat: add primitives textarea
    
    * feat: add primitives datepicker
    
    * feat: remove use client directives

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,2 +1,7 @@
 export * from "./button";
 export * from "./card";
+export * from "./input";
+export * from "./select";
+export * from "./checkbox";
+export * from "./textarea";
+export * from "./datepicker";
