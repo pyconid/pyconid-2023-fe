cce66c98399139636e65d76d476c32726a919b15
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-11-01 13:43:38 +0700

    feat: adjust content as docs

--- app/routes/sponsor._index.tsx
+++ app/routes/sponsor._index.tsx
@@ -8,34 +8,47 @@ export const meta: V2_MetaFunction = () => {
 }
 
 const sponsors = {
+  supporter: [
+    {
+      logo: "/python.svg",
+      alt: "logo_googlecloud",
+      href: "https://python.org",
+      description:
+        "The Python Software Foundation actively supports and promotes the Python programming language and its global community of users and developers. They play a vital role in maintaining Python, nurturing a sense of community, providing grants and sponsorships, and advocating for Python's utilization across various industries.",
+    },
+  ],
   platinum: [
     {
-      logo: "googlecloud.svg",
+      logo: "/googlecloud.svg",
       alt: "logo_googlecloud",
+      href: "https://cloud.google.com/",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Velit sit pellentesque ut porttitor. Duis pellentesque felis imperdiet lorem non. A diam nunc posuere sed proin massa in praesent eu. Sagittis sollicitudin placerat iaculis in hac. Quam vulputate scelerisque sit iaculis posuere nec facilisi gravida nec. Blandit urna enim rhoncus arcu posuere ullamcorper porttitor. Quis non massa ut in ultricies iaculis ut.",
+        "Google Cloud accelerates every organization’s ability to digitally transform its business and industry. We deliver enterprise-grade solutions that leverage Google’s cutting-edge technology, and tools that help developers build more sustainably. Customers in more than 200 countries and territories turn to Google Cloud as their trusted partner to enable growth and solve their most critical business problems.",
     },
   ],
   gold: [
     {
-      logo: "nginx.svg",
-      alt: "logo_nginx",
+      logo: "/julo.svg",
+      alt: "logo_julo",
+      href: "https://www.julo.co.id/",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Velit sit pellentesque ut porttitor. Duis pellentesque felis imperdiet lorem non. A diam nunc posuere sed proin massa in praesent eu. Sagittis sollicitudin placerat iaculis in hac. Quam vulputate scelerisque sit iaculis posuere nec facilisi gravida nec. Blandit urna enim rhoncus arcu posuere ullamcorper porttitor. Quis non massa ut in ultricies iaculis ut.",
+        "JULO is a financial technology company that is working on a financial super app. JULO started off with the lending arm and have excelled at it. Hundreds of thousands of Indonesians now use JULO apps to get access to a decent credit product. It's a mission to help millions of Indonesians get a better life. That's how we'll build a company future generations will be proud of. JULO are now tapping into other financial territories, to eventually provide a one stop financial solution to all Indonesians.",
     },
     {
-      logo: "julo.svg",
-      alt: "logo_julo",
+      logo: "/nginx.svg",
+      alt: "logo_nginx",
+      href: "https://www.nginx.com/",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Velit sit pellentesque ut porttitor. Duis pellentesque felis imperdiet lorem non. A diam nunc posuere sed proin massa in praesent eu. Sagittis sollicitudin placerat iaculis in hac. Quam vulputate scelerisque sit iaculis posuere nec facilisi gravida nec. Blandit urna enim rhoncus arcu posuere ullamcorper porttitor. Quis non massa ut in ultricies iaculis ut.",
+        "NGINX, now a part of F5, Inc., is the company behind the popular open source project, NGINX. F5 NGINX delivers cloud-native, Kubernetes-friendly open source and enterprise solutions that drive mission-critical apps and APIs with scalability, visibility, security, and governance.",
     },
   ],
   silver: [
     {
-      logo: "ocbcnisp.svg",
+      logo: "/ocbcnisp.svg",
       alt: "logo_ocbnisp",
+      href: "https://www.ocbcnisp.com/",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Velit sit pellentesque ut porttitor. Duis pellentesque felis imperdiet lorem non. A diam nunc posuere sed proin massa in praesent eu. Sagittis sollicitudin placerat iaculis in hac. Quam vulputate scelerisque sit iaculis posuere nec facilisi gravida nec. Blandit urna enim rhoncus arcu posuere ullamcorper porttitor. Quis non massa ut in ultricies iaculis ut.",
+        "As a bank, OCBC NISP believes that success comes from trust and collaboration. With the value “no such things as can’t”, we grow with our employees and partners to go far together.In career, we encourage everyone to build progress and value together with us, because #OpportunityStartsHere.",
     },
   ],
 }
@@ -58,6 +71,27 @@ export default function Index() {
           role="separator"
         />
         <div className="mx-auto w-fit rounded-lg bg-primary-purple p-2 text-xl font-bold text-white lg:p-3">
+          Supported by
+        </div>
+        {sponsors.supporter.map((sponsor, index) => (
+          <div
+            key={index}
+            className="mx-auto mt-4 flex flex-col items-center lg:w-1/2"
+          >
+            <div className="h-[100px]">
+              <a href={sponsor.href} target="_blank" rel="noopener noreferrer">
+                <img src={sponsor.logo} alt={sponsor.alt} />
+              </a>
+            </div>
+
+            <p>{sponsor.description}</p>
+          </div>
+        ))}
+        <div
+          className="mb-5 mt-16 h-2 rounded bg-primary-red lg:mb-7"
+          role="separator"
+        />
+        <div className="mx-auto w-fit rounded-lg bg-primary-red p-2 text-xl font-bold text-white lg:p-3">
           Platinum Sponsor
         </div>
         {sponsors.platinum.map((sponsor, index) => (
@@ -65,7 +99,9 @@ export default function Index() {
             key={index}
             className="mx-auto mt-4 flex flex-col items-center lg:w-1/2"
           >
-            <img src={sponsor.logo} alt={sponsor.alt} />
+            <a href={sponsor.href} target="_blank" rel="noopener noreferrer">
+              <img src={sponsor.logo} alt={sponsor.alt} />
+            </a>
             <p>{sponsor.description}</p>
           </div>
         ))}
@@ -83,17 +119,23 @@ export default function Index() {
               className="mx-auto flex flex-col items-center lg:w-2/3"
             >
               <div className="h-[80px]">
-                <img src={sponsor.logo} alt={sponsor.alt} />
+                <a
+                  href={sponsor.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={sponsor.logo} alt={sponsor.alt} />
+                </a>
               </div>
               <p>{sponsor.description}</p>
             </div>
           ))}
         </div>
         <div
-          className="mb-5 mt-16 h-2 rounded bg-primary-red lg:mb-7"
+          className="mb-5 mt-16 h-2 rounded bg-primary-black lg:mb-7"
           role="separator"
         />
-        <div className="mx-auto w-fit rounded-lg bg-primary-red p-2 text-xl font-bold text-white lg:p-3">
+        <div className="mx-auto w-fit rounded-lg bg-primary-black p-2 text-xl font-bold text-white lg:p-3">
           Silver Sponsor
         </div>
         {sponsors.silver.map((sponsor, index) => (
@@ -102,7 +144,9 @@ export default function Index() {
             className="mx-auto mt-4 flex flex-col items-center lg:w-1/3"
           >
             <div className="h-[80px]">
-              <img src={sponsor.logo} alt={sponsor.alt} />
+              <a href={sponsor.href} target="_blank" rel="noopener noreferrer">
+                <img src={sponsor.logo} alt={sponsor.alt} />
+              </a>
             </div>
             <p>{sponsor.description}</p>
           </div>
