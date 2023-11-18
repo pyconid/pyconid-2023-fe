e3050bf88e45df0a9b175d4523bbabbfde2898e1
pardi123 <pardi@zero-one-group.com>
2023-11-18 11:36:56 +0700

    wip

--- app/routes/org._index.tsx
+++ app/routes/org._index.tsx
@@ -1,15 +1,59 @@
-import type { V2_MetaFunction } from "@remix-run/node"
+import { json, type V2_MetaFunction } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
 
 import { Layout } from "~/components"
 import { ChairmanCard } from "~/components/organizer/chairman"
 import { CommiteeCard } from "~/components/organizer/commitee"
 import { VolunteerCard } from "~/components/organizer/volunteer"
+import { prisma } from "~/db.server"
+
+
+export async function loader() {
+  const comitee = await prisma.participantType.findMany({
+    where: {
+      symbol: {
+        in: ['LEAD_ORGANIZER', 'COMMITEE', 'BINUS_BANDUNG_VOLUNTEER']
+      }
+    }
+  })
+  if (comitee) {
+    const organizer = await prisma.user.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        participantType: true,
+        twitter: true,
+        github: true,
+      }, where: {
+        participantTypeId: {
+
+          in: comitee.map(data => data.id)
+        }
+      }
+    });
+    const leadOrganizer = organizer.filter((data) => {
+      return data.participantType?.symbol === "LEAD_ORGANIZER"
+    })
+    return json({
+      leadOrganizer: leadOrganizer,
+      organizer: organizer
+    })
+  }
+
+  return json({
+    leadOrganizer: [],
+    organizer: []
+  })
+}
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Organizer" }]
 }
-
 export default function Index() {
+  const { organizer, leadOrganizer } = useLoaderData<typeof loader>()
+  console.log(leadOrganizer)
+
+
   return (
     <Layout>
       <div className="pb-10 pt-5 text-center font-brand text-primary lg:py-20">
@@ -25,21 +69,26 @@ export default function Index() {
         <img src="/yellow-pycon-org.svg" alt="" />
       </div>
       <div className="mx-auto flex max-w-7xl px-4 md:px-6">
-        <ChairmanCard id="1" firstName="Dima" lastName="Maharika D">
-          <div className="max-w-2xl">
-            <p>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. Loves rabbits, cars,
-              drones, and gadgets such as Raspberry Pi as well. Currently taking
-              special.
-            </p>
-            <div className="mt-4 flex justify-center gap-3 lg:justify-start">
-              <img src="icons/github.svg" />
-              <img src="icons/x.svg" />
-            </div>
-          </div>
-        </ChairmanCard>
+        {leadOrganizer.map((data) => {
+          return (
+            <ChairmanCard id="1" firstName="Dima" lastName="Maharika D">
+              <div className="max-w-2xl">
+                <p>
+                  Lorem Ipsum has been the industry's standard dummy text ever since
+                  the 1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book. Loves rabbits, cars,
+                  drones, and gadgets such as Raspberry Pi as well. Currently taking
+                  special.
+                </p>
+                <div className="mt-4 flex justify-center gap-3 lg:justify-start">
+                  <img src="icons/github.svg" />
+                  <img src="icons/x.svg" />
+                </div>
+              </div>
+            </ChairmanCard>
+          )
+        })}
+
       </div>
 
       <div className="mb-12 flex items-end justify-center">
