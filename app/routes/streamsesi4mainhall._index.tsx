c5e21a8c68a5ad5959a2638459650f91aad0d01f
Dima M Dinama <dmaharika@gmail.com>
2023-11-18 14:52:31 +0700

    add : stream link

---
+++ app/routes/streamsesi4mainhall._index.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "@remix-run/node"
+
+export function loader() {
+  return redirect(
+    "https://streamyard.com/qpxh6bpyzw",
+  )
+}
---
+++ app/routes/streamsesi5mainhall._index.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "@remix-run/node"
+
+export function loader() {
+  return redirect(
+    "https://streamyard.com/gnjr2k5rfe",
+  )
+}
