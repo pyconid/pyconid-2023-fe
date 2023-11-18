d5a2e4860dc14afec5861f0e24f4187f79d614b4
Dima M Dinama <dmaharika@gmail.com>
2023-11-18 23:32:22 +0700

    delete: unused redirect link

--- closingday1._index.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@remix-run/node"
-
-export function loader() {
-  return redirect(
-    "https://docs.google.com/presentation/d/1MjIa5n9gYeCw5y5uu_SYJ4c19dObFsJIhDNRCpVBEL8/edit?usp=sharing",
-  )
-}
