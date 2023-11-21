21de842253680c9c0124a5633d75f7495e5a13f5
Dima M Dinama <dmaharika@gmail.com>
2023-11-21 14:21:59 +0700

    fix : link for ecommerce

--- app/routes/offlinemerch._index.tsx
+++ app/routes/offlinemerch._index.tsx
@@ -2,6 +2,6 @@ import { redirect } from "@remix-run/node"
 
 export function loader() {
   return redirect(
-    "https://docs.google.com/presentation/d/1ISHqcCmVMM4JHGR6DDbJ_HgSFPf3UNfrX7q8LHDbpmQ/edit?usp=sharing",
+    "https://www.tokopedia.com/pyconid/paket-merchandise-general-in-person-pyconid-2023",
   )
 }
